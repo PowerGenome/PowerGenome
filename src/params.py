758f43e89da3cd851083000c17f9bbbfca6612e9
Greg Schivley <greg.schivley@gmail.com>
2019-08-15 10:50:54 -0400

    Add shapefile paths

--- src/params.py
+++ src/params.py
@@ -12,3 +12,7 @@ project_path = src_path.parent
 DATA_PATHS = {}
 DATA_PATHS["results"] = project_path / "results"
 DATA_PATHS['src'] = project_path / "src"
+DATA_PATHS['data'] = project_path / 'data'
+DATA_PATHS['ipm_shapefiles'] = DATA_PATHS['data'] / "IPM Regions v617 04-05-17"
+
+IPM_SHAPEFILE_PATH = DATA_PATHS['ipm_shapefiles'] / "IPM_Regions_201770405.shp"
