5c192163779aa7c14566f298ea293502997b0ec9
Greg Schivley <greg.schivley@gmail.com>
2019-10-09 17:29:53 -0400

    Add new generators based on settings

--- powergenome/nrelatb.py
+++ powergenome/nrelatb.py
@@ -3,6 +3,7 @@ Functions to fetch and modify NREL ATB data from PUDL
 """
 
 import logging
+import numpy as np
 import pandas as pd
 
 idx = pd.IndexSlice
@@ -165,3 +166,107 @@ def atb_fixed_var_om_existing(results, atb_costs_df, atb_hr_df, settings):
     mod_results = mod_results.sort_values(["region", "technology", "cluster"])
 
     return mod_results
+
+
+def single_generator_row(atb_costs, new_gen_type, model_year):
+
+    technology, tech_detail, cost_case, size_mw = new_gen_type
+    row = atb_costs.query(
+        "technology==@technology & tech_detail==@tech_detail "
+        "& cost_case==@cost_case & basis_year==@model_year"
+    ).copy()
+    row["Cap_size"] = size_mw
+
+    return row
+
+
+def investment_cost_calculator(capex, wacc, cap_rec_years):
+
+    inv_cost = capex * (
+        np.exp(wacc * cap_rec_years)
+        * (np.exp(wacc) - 1)
+        / (np.exp(wacc * cap_rec_years) - 1)
+    )
+
+    return inv_cost
+
+
+def atb_new_generators(results, atb_costs, atb_hr, settings):
+    """Add rows for new generators in each region
+
+    Parameters
+    ----------
+    results : DataFrame
+        Compiled results of clustered power plants with weighted average heat
+    atb_costs : [type]
+        [description]
+    atb_hr : [type]
+        [description]
+    settings : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+
+    new_gen_types = settings["atb_new_gen"]
+    model_year = settings["model_year"]
+    regions = settings["model_regions"]
+
+    new_gen_df = pd.concat(
+        [
+            single_generator_row(atb_costs, new_gen, model_year)
+            for new_gen in new_gen_types
+        ],
+        ignore_index=True,
+    )
+
+    new_gen_df["Inv_cost_per_MWyr"] = investment_cost_calculator(
+        capex=new_gen_df["capex"],
+        wacc=new_gen_df["waccnomtech"],
+        cap_rec_years=settings["atb_cap_recovery_years"],
+    )
+
+    new_gen_df = new_gen_df.merge(
+        atb_hr, on=["technology", "tech_detail", "basis_year"], how="left"
+    )
+
+    new_gen_df = new_gen_df.rename(
+        columns={
+            "heat_rate": "Heat_rate_MMBTU_per_MWh",
+            "o_m_fixed_mw": "Fixed_OM_cost_per_MWyr",
+            "o_m_variable_mwh": "Var_OM_cost_per_MWh",
+        }
+    )
+
+    new_gen_df["technology"] = (
+        new_gen_df["technology"]
+        + "_"
+        + new_gen_df["tech_detail"]
+        + "_"
+        + new_gen_df["cost_case"]
+    )
+
+    keep_cols = [
+        "technology",
+        "basis_year",
+        # "tech_detail",
+        "Fixed_OM_cost_per_MWyr",
+        "Var_OM_cost_per_MWh",
+        # "fuel",
+        "Inv_cost_per_MWyr",
+        "Heat_rate_MMBTU_per_MWh",
+        "Cap_size"
+    ]
+
+    df_list = []
+    for region in regions:
+        _df = new_gen_df.loc[:, keep_cols].copy()
+        _df["region"] = region
+        df_list.append(_df)
+
+    results = pd.concat([results, pd.concat(df_list, ignore_index=True)], ignore_index=True)
+
+    return results
