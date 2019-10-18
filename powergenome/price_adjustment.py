dc559321f84ec595614b4c543ee29fb04041a5cb
Greg Schivley <greg.schivley@gmail.com>
2019-10-18 11:54:06 -0400

    Create price adjustment function using CPI
    Add cpi package to environment and requirements

---
+++ powergenome/price_adjustment.py
@@ -0,0 +1,25 @@
+"""
+Adjust price/cost from one year to another
+"""
+
+import logging
+
+import cpi
+
+logger = logging.getLogger(__name__)
+
+
+def inflation_price_adjustment(price, base_year, target_year):
+
+    if cpi.LATEST_YEAR < target_year:
+        logger.info("Updating CPI data")
+        cpi.update()
+        if cpi.LATEST_YEAR < target_year:
+            raise ValueError(
+                f"CPI data are only available through {cpi.LATEST_YEAR}. Your target year is "
+                f"{target_year}"
+            )
+
+    price = price * cpi.inflate(1, base_year, to=target_year)
+
+    return price
