2562f7a865a262b26cab02e6fc3f635f57916c00
Greg Schivley <greg.schivley@gmail.com>
2020-01-23 13:15:56 -0500

    update CLI command name and example settings file

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     description="Extract PUDL data for use in power system models",
     author="Greg Schivley",
     entry_points={
-        "console_scripts": ["powergenome_data = powergenome.run_powergenome_cli:main"]
+        "console_scripts": ["run_powergenome = powergenome.run_powergenome_cli:main"]
     },
 )
