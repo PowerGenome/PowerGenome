1f2a29910ca42bb6b38fe90fe043b5ce7f2c8666
Greg Schivley <greg.schivley@gmail.com>
2020-03-26 12:43:49 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="powergenome",
     packages=find_packages(),
-    version="0.1.1",
+    version="0.2.0",
     description="Extract PUDL data for use in power system models",
     author="Greg Schivley",
     entry_points={
