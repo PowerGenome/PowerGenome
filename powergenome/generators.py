PowerGenome/PowerGenome

commit 1a6045059af996c545913198f6e55dda2b0f5fc9

Simplify and fix settings logic
- Access optional key with `settings.get(key)`
- Iterate on key with `settings.get(key, [])` or `settings.get(key, {})`

powergenome/generators.py
@@ -153,8 +153,8 @@ def startup_fuel(df, settings):
         All generator clusters. Must have a column "technology". Can include both EIA
         and NRELATB technology names.
     settings : dictionary
-        User-defined settings loaded from a YAML file. Must have keys "startup_fuel_use"
-        and "eia_atb_tech_map".
+        User-defined settings loaded from a YAML file. Keys in "startup_fuel_use"
+        must match those in "eia_atb_tech_map".
 
     Returns
     -------
@@ -162,7 +162,7 @@ def startup_fuel(df, settings):
         Modified dataframe with the new column "Start_fuel_MMBTU_per_MW".
     """
     df["Start_fuel_MMBTU_per_MW"] = 0
-    for eia_tech, fuel_use in settings["startup_fuel_use"].items():
+    for eia_tech, fuel_use in settings.get("startup_fuel_use", {}).items():
         atb_tech = settings["eia_atb_tech_map"][eia_tech].split("_")[0]
 
         df.loc[df["technology"] == eia_tech, "Start_fuel_MMBTU_per_MW"] = fuel_use
@@ -254,23 +254,22 @@ def group_technologies(df, settings):
     dataframe
         Same as incoming dataframe but with grouped technology types
     """
-    if settings["group_technologies"] is True:
+    if settings.get("group_technologies"):
 
         df["_technology"] = df["technology_description"]
         for tech, group in settings["tech_groups"].items():
             df.loc[df["technology_description"].isin(group), "_technology"] = tech
 
-        if settings["regional_no_grouping"] is not None:
-            for region, tech_list in settings["regional_no_grouping"].items():
-                df.loc[
-                    (df["model_region"] == region)
-                    & (df["technology_description"].isin(tech_list)),
-                    "_technology",
-                ] = df.loc[
-                    (df["model_region"] == region)
-                    & (df["technology_description"].isin(tech_list)),
-                    "technology_description",
-                ]
+        for region, tech_list in settings.get("regional_no_grouping", {}).items():
+            df.loc[
+                (df["model_region"] == region)
+                & (df["technology_description"].isin(tech_list)),
+                "_technology",
+            ] = df.loc[
+                (df["model_region"] == region)
+                & (df["technology_description"].isin(tech_list)),
+                "technology_description",
+            ]
 
         df.loc[:, "technology_description"] = df.loc[:, "_technology"]
         df = df.drop(columns=["_technology"])
@@ -456,7 +455,7 @@ def label_retirement_year(
         pass
 
     # Add additonal retirements from settings file
-    if settings["additional_retirements"] and add_additional_retirements:
+    if settings.get("additional_retirements") and add_additional_retirements:
         logger.info("Changing retirement dates based on settings file")
         model_year = settings["model_year"]
         start_ret_cap = df.loc[
@@ -529,60 +528,60 @@ def label_small_hydro(df, settings, by=["plant_id_eia"]):
         If the user wants to label small hydro plants, some of the conventional
         hydro facilities will have their technology type changed to small hydro.
     """
-    if settings["small_hydro"] is True:
-        if "report_date" not in by and "report_date" in df.columns:
-            # by.append("report_date")
-            logger.warning("'report_date' is in the df but not used in the groupby")
-        region_agg_map = reverse_dict_of_lists(settings["region_aggregations"])
-        keep_regions = [
-            x
-            for x in settings["model_regions"] + list(region_agg_map)
-            if x in settings["small_hydro_regions"]
-        ]
-        start_len = len(df)
-        size_cap = settings["small_hydro_mw"]
-        if settings["capacity_col"] in df:
-            cap_col = settings["capacity_col"]
-        else:
-            cap_col = "capacity_mw"
+    if not settings.get("small_hydro"):
+        return None
+    if "report_date" not in by and "report_date" in df.columns:
+        # by.append("report_date")
+        logger.warning("'report_date' is in the df but not used in the groupby")
+    region_agg_map = reverse_dict_of_lists(settings["region_aggregations"])
+    keep_regions = [
+        x
+        for x in settings["model_regions"] + list(region_agg_map)
+        if x in settings["small_hydro_regions"]
+    ]
+    start_len = len(df)
+    size_cap = settings["small_hydro_mw"]
+    cap_col = settings.get("capacity_col")
+    if not cap_col in df:
+        cap_col = "capacity_mw"
 
-        start_hydro_capacity = df.query(
-            "technology_description=='Conventional Hydroelectric'"
-        )[cap_col].sum()
+    start_hydro_capacity = df.query(
+        "technology_description=='Conventional Hydroelectric'"
+    )[cap_col].sum()
 
-        plant_capacity = (
-            df.loc[
-                (df["technology_description"] == "Conventional Hydroelectric")
-                & (df["model_region"].isin(keep_regions))
-            ]
-            .groupby(by, as_index=False)[cap_col]
-            .sum()
-        )
-
-        small_hydro_plants = plant_capacity.loc[
-            plant_capacity[cap_col] <= size_cap, "plant_id_eia"
-        ]
-
+    plant_capacity = (
         df.loc[
             (df["technology_description"] == "Conventional Hydroelectric")
-            & (df["plant_id_eia"].isin(small_hydro_plants)),
-            "technology_description",
-        ] = "Small Hydroelectric"
+            & (df["model_region"].isin(keep_regions))
+        ]
+        .groupby(by, as_index=False)[cap_col]
+        .sum()
+    )
 
-        end_len = len(df)
-        small_hydro_capacity = df.query(
-            "technology_description=='Small Hydroelectric'"
-        )[cap_col].sum()
-        end_conv_hydro_capacity = df.query(
-            "technology_description=='Conventional Hydroelectric'"
-        )[cap_col].sum()
+    small_hydro_plants = plant_capacity.loc[
+        plant_capacity[cap_col] <= size_cap, "plant_id_eia"
+    ]
 
-        assert start_len == end_len
-        assert np.allclose(
-            start_hydro_capacity, small_hydro_capacity + end_conv_hydro_capacity
-        )
+    df.loc[
+        (df["technology_description"] == "Conventional Hydroelectric")
+        & (df["plant_id_eia"].isin(small_hydro_plants)),
+        "technology_description",
+    ] = "Small Hydroelectric"
 
-        return df
+    end_len = len(df)
+    small_hydro_capacity = df.query("technology_description=='Small Hydroelectric'")[
+        cap_col
+    ].sum()
+    end_conv_hydro_capacity = df.query(
+        "technology_description=='Conventional Hydroelectric'"
+    )[cap_col].sum()
+
+    assert start_len == end_len
+    assert np.allclose(
+        start_hydro_capacity, small_hydro_capacity + end_conv_hydro_capacity
+    )
+
+    return df
 
 
 def load_generator_860_data(pudl_engine, data_years=[2017]):
@@ -1376,20 +1375,17 @@ def import_proposed_generators(planned, settings, model_regions_gdf):
     planned_gdf = gpd.sjoin(model_regions_gdf.drop(columns="IPM_Region"), planned_gdf)
 
     # Add planned additions from the settings file
-    if settings["additional_planned"]:
-        i = 0
-        for record in settings["additional_planned"]:
-            plant_id, gen_id, model_region = record
-            plant_record = planned.loc[
-                (planned["plant_id_eia"] == plant_id)
-                & (planned["generator_id"] == gen_id),
-                :,
-            ]
-            plant_record["model_region"] = model_region
+    for i, record in enumerate(settings.get("additional_planned", [])):
+        plant_id, gen_id, model_region = record
+        plant_record = planned.loc[
+            (planned["plant_id_eia"] == plant_id) & (planned["generator_id"] == gen_id),
+            :,
+        ]
+        plant_record["model_region"] = model_region
 
-            planned_gdf = planned_gdf.append(plant_record, sort=False)
-            i += 1
-        logger.info(f"{i} generators were added to the planned list based on settings")
+        planned_gdf = planned_gdf.append(plant_record, sort=False)
+
+    logger.info(f"{i} generators were added to the planned list based on settings")
 
     planned_gdf.loc[:, "heat_rate_mmbtu_mwh"] = planned_gdf.loc[
         :, "technology_description"
@@ -1421,7 +1417,7 @@ def import_proposed_generators(planned, settings, model_regions_gdf):
         add_additional_retirements=False,
     )
 
-    if settings["group_technologies"] is True:
+    if settings.get("group_technologies"):
         planned_gdf = group_technologies(planned_gdf, settings)
         print(planned_gdf["technology_description"].unique().tolist())
 
@@ -1531,7 +1527,7 @@ def gentype_region_capacity_factor(
         plant_tech_cap, generation, on=["plant_id_eia"], how="left"
     )
 
-    if settings["group_technologies"] is True:
+    if settings.get("group_technologies"):
         capacity_factor = group_technologies(capacity_factor, settings)
 
     if years_filter is None:
@@ -1623,7 +1619,7 @@ def add_fuel_labels(df, fuel_prices, settings):
     """
 
     df["Fuel"] = "None"
-    for eia_tech, fuel in settings["tech_fuel_map"].items():
+    for eia_tech, fuel in settings.get("tech_fuel_map", {}).items():
         try:
             if eia_tech == "Natural Gas Steam Turbine":
                 # No ATB natural gas steam turbine and I match it with coal for O&M
@@ -1659,7 +1655,7 @@ def add_fuel_labels(df, fuel_prices, settings):
                     "Fuel",
                 ] = fuel_name
 
-    for ccs_tech, ccs_fuel in settings["ccs_fuel_map"].items():
+    for ccs_tech, ccs_fuel in settings.get("ccs_fuel_map", {}).items():
         scenario = settings["aeo_fuel_scenarios"][ccs_fuel.split("_")[0]]
         for aeo_region, model_regions in settings["aeo_fuel_region_map"].items():
             ccs_fuel_name = ("_").join([aeo_region, scenario, ccs_fuel])
@@ -1906,7 +1902,7 @@ class GeneratorClusters:
         df_list = []
         self.demand_response_profiles = {}
 
-        if self.settings["demand_response_resources"] is not None:
+        if self.settings.get("demand_response_resources"):
             for resource, parameters in self.settings["demand_response_resources"][
                 year
             ].items():
@@ -2119,9 +2115,7 @@ class GeneratorClusters:
         logger.info("Creating technology clusters by region")
         unit_list = []
         self.cluster_list = []
-        alt_cluster_method = self.settings["alt_cluster_method"]
-        if alt_cluster_method is None:
-            alt_cluster_method = {}
+        alt_cluster_method = self.settings.get("alt_cluster_method", {})
 
         for _, df in region_tech_grouped:
             region, tech = _
@@ -2130,7 +2124,7 @@ class GeneratorClusters:
             # This is bad. Should be setting up a dictionary of objects that picks the
             # correct clustering method. Can't keep doing if statements as the number of
             # methods grows. CHANGE LATER.
-            if self.settings["alt_cluster_method"] is None:
+            if not alt_cluster_method:
                 if num_clusters[region][tech] > 0:
                     clusters = cluster.KMeans(
                         n_clusters=num_clusters[region][tech], random_state=6
@@ -2141,11 +2135,9 @@ class GeneratorClusters:
                     )  # Change to 1-index for julia
 
             else:
-                if (region in self.settings[alt_cluster_method].keys()) and (
-                    tech
-                    in self.settings[alt_cluster_method][region][
-                        "technology_description"
-                    ]
+                if (
+                    region in alt_cluster_method
+                    and tech in alt_cluster_method[region]["technology_description"]
                 ):
 
                     grouped = cluster_by_owner(
@@ -2219,7 +2211,7 @@ class GeneratorClusters:
                 how="left",
             )
 
-            if self.settings["derate_capacity"] is True:
+            if self.settings.get("derate_capacity"):
                 derate_techs = self.settings["derate_techs"]
                 self.results.loc[:, "unmodified_cap_size"] = self.results.loc[
                     :, "Cap_size"
@@ -2284,7 +2276,7 @@ class GeneratorClusters:
                 ["region", "technology"]
             )
 
-        if "capacity_limit_spur_line_fn" in self.settings:
+        if self.settings.get("capacity_limit_spur_line_fn"):
             self.new_generators = (
                 self.new_generators.pipe(add_resource_max_cap_spur_line, self.settings)
                 .pipe(calculate_transmission_inv_cost, self.settings)
@@ -2293,13 +2285,12 @@ class GeneratorClusters:
         else:
             logger.warning("No settings parameter for max capacity/spur line file")
 
-        if "demand_response_fn" in self.settings:
-            if self.settings["demand_response_fn"] is not None:
-                dr_rows = self.create_demand_response_gen_rows(
-                    scenario=self.settings["demand_response"]
-                )
+        if self.settings.get("demand_response_fn"):
+            dr_rows = self.create_demand_response_gen_rows(
+                scenario=self.settings["demand_response"]
+            )
 
-            self.new_generators = pd.concat([self.new_generators, dr_rows])
+        self.new_generators = pd.concat([self.new_generators, dr_rows])
 
         # self.new_generators = self.new_generators.rename(
         #     columns={"technology": "Resource"}

powergenome/GenX.py
@@ -138,10 +138,7 @@ def make_genx_settings_file(pudl_engine, settings, calculated_ces=None):
     genx_settings = load_settings(settings["genx_settings_fn"])
     policies = load_policy_scenarios(settings)
     year_case_policy = policies.loc[(case_id, model_year), :]
-    if (
-        "distributed_gen_profiles_fn" in settings
-        and settings["distributed_gen_profiles_fn"] is not None
-    ):
+    if settings.get("distributed_gen_profiles_fn"):
         dg_generation = make_distributed_gen_profiles(pudl_engine, settings)
         total_dg_gen = dg_generation.sum().sum()
     else:
@@ -181,7 +178,7 @@ def make_genx_settings_file(pudl_engine, settings, calculated_ces=None):
             genx_settings["CES"] = 3
 
             # This is a little confusing but for partial CES
-            if settings["partial_ces"] is True:
+            if settings.get("partial_ces"):
                 genx_settings["CES_Adjustment"] = 0
             else:
                 genx_settings["CES_Adjustment"] = float((1 - CES) * total_dg_gen)
@@ -193,7 +190,7 @@ def make_genx_settings_file(pudl_engine, settings, calculated_ces=None):
         genx_settings["CES_Adjustment"] = 0
 
     # Don't wrap when time domain isn't reduced
-    if settings["reduce_time_domain"] is False:
+    if not settings.get("reduce_time_domain"):
         genx_settings["OperationWrapping"] = 0
 
     genx_settings["case_id"] = case_id
@@ -206,7 +203,7 @@ def make_genx_settings_file(pudl_engine, settings, calculated_ces=None):
 
     # Load user defined values for the genx settigns file. This overrides the
     # complicated logic above.
-    if "case_genx_settings_fn" in settings:
+    if settings.get("case_genx_settings_fn"):
         user_genx_settings = load_user_genx_settings(settings)
         user_case_settings = user_genx_settings.loc[(case_id, model_year), :]
         for key, value in user_case_settings.items():
@@ -222,7 +219,7 @@ def reduce_time_domain(
 
     demand_segments = load_demand_segments(settings)
 
-    if settings["reduce_time_domain"]:
+    if settings.get("reduce_time_domain"):
         days = settings["time_domain_days_per_period"]
         time_periods = settings["time_domain_periods"]
         include_peak_day = settings["include_peak_day"]
@@ -394,29 +391,28 @@ def set_int_cols(df):
 
 def calculate_partial_CES_values(gen_clusters, fuels, settings):
     gens = gen_clusters.copy()
-    if "partial_ces" in settings:
-        if settings["partial_ces"] is True:
-            assert set(fuels["Fuel"]) == set(gens["Fuel"])
-            fuel_emission_map = fuels.copy()
-            fuel_emission_map = fuel_emission_map.set_index("Fuel")
+    if settings.get("partial_ces"):
+        assert set(fuels["Fuel"]) == set(gens["Fuel"])
+        fuel_emission_map = fuels.copy()
+        fuel_emission_map = fuel_emission_map.set_index("Fuel")
 
-            gens["co2_emission_rate"] = gens["Heat_rate_MMBTU_per_MWh"] * gens[
-                "Fuel"
-            ].map(fuel_emission_map["CO2_content_tons_per_MMBtu"])
+        gens["co2_emission_rate"] = gens["Heat_rate_MMBTU_per_MWh"] * gens["Fuel"].map(
+            fuel_emission_map["CO2_content_tons_per_MMBtu"]
+        )
 
-            # Make the partial CES credit equal to 1 ton minus the emissions rate, but
-            # don't include coal plants
+        # Make the partial CES credit equal to 1 ton minus the emissions rate, but
+        # don't include coal plants
 
-            partial_ces = 1 - gens["co2_emission_rate"]
+        partial_ces = 1 - gens["co2_emission_rate"]
 
-            gens.loc[
-                ~(gens["Resource"].str.contains("coal"))
-                & (gens["STOR"] == 0)
-                & (gens["DR"] == 0),
-                # & ~(gens["Resource"].str.contains("battery"))
-                # & ~(gens["Resource"].str.contains("load_shifting")),
-                "CES",
-            ] = partial_ces.round(3)
+        gens.loc[
+            ~(gens["Resource"].str.contains("coal"))
+            & (gens["STOR"] == 0)
+            & (gens["DR"] == 0),
+            # & ~(gens["Resource"].str.contains("battery"))
+            # & ~(gens["Resource"].str.contains("load_shifting")),
+            "CES",
+        ] = partial_ces.round(3)
     # else:
     #     gen_clusters = add_genx_model_tags(gen_clusters, settings)
 

powergenome/load_profiles.py
@@ -80,11 +80,10 @@ def add_load_growth(load_curves, settings):
         for ipm_region, load_region in load_map.items()
     }
 
-    if settings["alt_growth_rate"] is not None:
-        for region, rate in settings["alt_growth_rate"].items():
-            load_growth_map[region] = rate
+    for region, rate in settings.get("alt_growth_rate", {}).items():
+        load_growth_map[region] = rate
 
-    if "regular_load_growth_start_year" in settings.keys():
+    if settings.get("regular_load_growth_start_year"):
         # historical load growth
 
         demand_start = settings["aeo_hist_start_elec_demand"]
@@ -176,14 +175,14 @@ def make_final_load_curves(
         pudl_engine, settings, pudl_table, settings_agg_key
     )
 
-    if "demand_response_fn" in settings.keys():
+    if settings.get("demand_response_fn"):
         load_curves_dr = add_demand_response_resource_load(
             load_curves_before_dg, settings
         )
     else:
         load_curves_dr = load_curves_before_dg
 
-    if "distributed_gen_profiles_fn" in settings.keys():
+    if settings.get("distributed_gen_profiles_fn"):
         final_load_curves = subtract_distributed_generation(
             load_curves_dr, pudl_engine, settings
         )

powergenome/nrelatb.py
@@ -571,8 +571,8 @@ def atb_new_generators(atb_costs, atb_hr, settings):
     # Add user-defined technologies
     # This should probably be separate from ATB techs, and the regional cost multipliers
     # should be its own function.
-    if settings["additional_technologies_fn"] is not None:
-        if isinstance(settings["additional_new_gen"], list):
+    if settings.get("additional_technologies_fn"):
+        if isinstance(settings.get("additional_new_gen"), list):
             # user_costs, user_hr = load_user_defined_techs(settings)
             user_tech = load_user_defined_techs(settings)
             # new_gen_df = pd.concat([new_gen_df, user_costs], ignore_index=True, sort=False)
@@ -586,7 +586,7 @@ def atb_new_generators(atb_costs, atb_hr, settings):
                 " were specified in the settings file."
             )
 
-    if settings["modified_atb_new_gen"] is not None:
+    if settings.get("modified_atb_new_gen"):
         modified_gens = add_modified_atb_generators(
             settings, atb_costs_hr, model_year_range
         )
@@ -604,7 +604,7 @@ def atb_new_generators(atb_costs, atb_hr, settings):
     # Adjust values for CT/CC generators to match advanced techs in NEMS rather than
     # ATB average of advanced and conventional.
     # This is now generalized for changes to ATB values for any technology type.
-    for tech, _tech_modifiers in settings["atb_modifiers"].items():
+    for tech, _tech_modifiers in settings.get("atb_modifiers", {}).items():
         tech_modifiers = copy.deepcopy(_tech_modifiers)
         assert isinstance(tech_modifiers, dict), (
             "The settings parameter 'atb_modifiers' must be a nested list.\n"
@@ -661,12 +661,11 @@ def atb_new_generators(atb_costs, atb_hr, settings):
 
     new_gen_df["cap_recovery_years"] = settings["atb_cap_recovery_years"]
 
-    if settings["alt_atb_cap_recovery_years"]:
-        for tech, years in settings["alt_atb_cap_recovery_years"].items():
-            new_gen_df.loc[
-                new_gen_df.technology.str.lower().str.contains(tech.lower()),
-                "cap_recovery_years",
-            ] = years
+    for tech, years in settings.get("alt_atb_cap_recovery_years", {}).items():
+        new_gen_df.loc[
+            new_gen_df.technology.str.lower().str.contains(tech.lower()),
+            "cap_recovery_years",
+        ] = years
 
     new_gen_df["Inv_cost_per_MWyr"] = investment_cost_calculator(
         capex=new_gen_df["capex"],
@@ -683,25 +682,22 @@ def atb_new_generators(atb_costs, atb_hr, settings):
     new_gen_df["cap_recovery_years"] = settings["atb_cap_recovery_years"]
 
     # Some technologies might have a different capital recovery period
-    if settings["alt_atb_cap_recovery_years"] is not None:
-        for tech, years in settings["alt_atb_cap_recovery_years"].items():
-            tech_mask = new_gen_df["technology"].str.contains(tech)
+    for tech, years in settings.get("alt_atb_cap_recovery_years", {}).items():
+        tech_mask = new_gen_df["technology"].str.contains(tech)
 
-            new_gen_df.loc[tech_mask, "cap_recovery_years"] = years
+        new_gen_df.loc[tech_mask, "cap_recovery_years"] = years
 
-            new_gen_df.loc[tech_mask, "Inv_cost_per_MWyr"] = investment_cost_calculator(
-                capex=new_gen_df.loc[tech_mask, "capex"],
-                wacc=new_gen_df.loc[tech_mask, "waccnomtech"],
-                cap_rec_years=years,
-            )
+        new_gen_df.loc[tech_mask, "Inv_cost_per_MWyr"] = investment_cost_calculator(
+            capex=new_gen_df.loc[tech_mask, "capex"],
+            wacc=new_gen_df.loc[tech_mask, "waccnomtech"],
+            cap_rec_years=years,
+        )
 
-            new_gen_df.loc[
-                tech_mask, "Inv_cost_per_MWhyr"
-            ] = investment_cost_calculator(
-                capex=new_gen_df.loc[tech_mask, "capex_mwh"],
-                wacc=new_gen_df.loc[tech_mask, "waccnomtech"],
-                cap_rec_years=years,
-            )
+        new_gen_df.loc[tech_mask, "Inv_cost_per_MWhyr"] = investment_cost_calculator(
+            capex=new_gen_df.loc[tech_mask, "capex_mwh"],
+            wacc=new_gen_df.loc[tech_mask, "waccnomtech"],
+            cap_rec_years=years,
+        )
 
     keep_cols = [
         "technology",
@@ -741,10 +737,7 @@ def atb_new_generators(atb_costs, atb_hr, settings):
         )
         _df = add_extra_wind_solar_rows(_df, region, settings)
 
-        if (
-            settings["new_gen_not_available"] is not None
-            and region in settings["new_gen_not_available"].keys()
-        ):
+        if region in settings.get("new_gen_not_available", {}).keys():
             techs = settings["new_gen_not_available"][region]
             for tech in techs:
                 _df = _df.loc[~_df["technology"].str.contains(tech), :]

powergenome/run_powergenome_multiple_outputs_cli.py
@@ -357,7 +357,7 @@ def main():
                     logger.info(
                         f"Finished first round with year {year} scenario {case_id}"
                     )
-                    # if "partial_ces" in settings:
+                    # if settings.get("partial_ces"):
                     gens = calculate_partial_CES_values(gen_clusters, fuels, _settings)
                     write_results_file(
                         df=remove_fuel_scenario_name(gens.fillna(0), _settings),
@@ -426,7 +426,7 @@ def main():
                     ).pipe(add_genx_model_tags, _settings)
 
                     gen_clusters = gc.create_all_generators()
-                    # if "partial_ces" in settings:
+                    # if settings.get("partial_ces"):
                     #     fuels = fuel_cost_table(
                     #         fuel_costs=gc.fuel_prices,
                     #         generators=gc.all_resources,

commit 5de14a2a2da28108a7acf7c47cb237f03cb869fc

Fix unreferenced variable in logger message
Bug introduced by 1a6045059af996c545913198f6e55dda2b0f5fc9

powergenome/generators.py
@@ -1375,7 +1375,8 @@ def import_proposed_generators(planned, settings, model_regions_gdf):
     planned_gdf = gpd.sjoin(model_regions_gdf.drop(columns="IPM_Region"), planned_gdf)
 
     # Add planned additions from the settings file
-    for i, record in enumerate(settings.get("additional_planned") or []):
+    additional_planned = settings.get("additional_planned") or []
+    for record in additional_planned:
         plant_id, gen_id, model_region = record
         plant_record = planned.loc[
             (planned["plant_id_eia"] == plant_id) & (planned["generator_id"] == gen_id),
@@ -1385,7 +1386,9 @@ def import_proposed_generators(planned, settings, model_regions_gdf):
 
         planned_gdf = planned_gdf.append(plant_record, sort=False)
 
-    logger.info(f"{i} generators were added to the planned list based on settings")
+    logger.info(
+        f"{len(additional_planned)} generators were added to the planned list based on settings"
+    )
 
     planned_gdf.loc[:, "heat_rate_mmbtu_mwh"] = planned_gdf.loc[
         :, "technology_description"