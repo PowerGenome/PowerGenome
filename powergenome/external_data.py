PowerGenome/PowerGenome

commit b99263b7db165dfced12c2d2029ef05294535534

Use interconnect_annuity as default transmission cost

powergenome/generators.py
@@ -1750,33 +1750,50 @@ def calculate_transmission_inv_cost(resource_df, settings):
     return resource_df
 
 
-def add_transmission_inv_cost(resource_df):
+def add_transmission_inv_cost(
+    resource_df: pd.DataFrame, settings: dict
+) -> pd.DataFrame:
     """Add tranmission investment costs to plant investment costs
 
     Parameters
     ----------
-    resource_df : DataFrame
+    resource_df
         Each row represents a single resource within a region. Should have columns
-        `Inv_cost_per_MWyr` and `<type>_inv_mwyr`, where transmission <type> is one of
-        'spur', 'offshore_spur', or 'tx'.
+        `Inv_cost_per_MWyr` and transmission costs.
+            - one or more `<type>_inv_mwyr`,
+                where <type> is 'spur', 'offshore_spur', or 'tx'.
+            - `interconnect_annuity`
+    settings
+        User settings. If `transmission_investment_cost.use_total` is present and true,
+        `interconnect_annuity` is used over `<type>_inv_mwys` if present, not null,
+        and not zero.
 
     Returns
     -------
     DataFrame
         A modified copy of the input dataframe where 'Inv_cost_per_MWyr' represents the
-        combined plant and spur line investment costs. The new column
+        combined plant and transmission investment costs. The new column
         `plant_inv_cost_mwyr` represents just the plant investment costs.
     """
+    use_total = (
+        settings.get("transmission_investment_cost", {}).get("use_total", False)
+        and "interconnect_annuity" in resource_df
+    )
+    resource_df["plant_inv_cost_mwyr"] = resource_df["Inv_cost_per_MWyr"]
     columns = [
-        f"{x}_inv_mwyr" for x in TRANSMISSION_TYPES if f"{x}_inv_mwyr" in resource_df
+        c for c in [f"{t}_inv_mwyr" for t in TRANSMISSION_TYPES] if c in resource_df
     ]
-    if not columns:
+    cost = resource_df[columns].sum(axis=1)
+    if use_total:
+        total = resource_df["interconnect_annuity"]
+        has_total = ~total.isna() & total != 0
+        cost[has_total] = total[has_total]
+    if cost.isna().any() or (cost == 0).any():
         logger.warning(
-            "Transmission investment costs are missing and will not be included "
-            "in the total investment costs."
+            "Transmission investment costs are missing or zero for some resources"
+            " and will not be included in the total investment costs."
         )
-    resource_df["plant_inv_cost_mwyr"] = resource_df.loc[:, "Inv_cost_per_MWyr"]
-    resource_df["Inv_cost_per_MWyr"] += resource_df.loc[:, columns].sum(axis=1)
+    resource_df["Inv_cost_per_MWyr"] += cost
     return resource_df
 
 
@@ -2286,7 +2303,7 @@ class GeneratorClusters:
             logger.warning("No settings parameter for max capacity/spur file")
         self.new_generators = self.new_generators.pipe(
             calculate_transmission_inv_cost, self.settings
-        ).pipe(add_transmission_inv_cost)
+        ).pipe(add_transmission_inv_cost, self.settings)
 
         if self.settings.get("demand_response_fn"):
             dr_rows = self.create_demand_response_gen_rows()

example_system/test_settings.yml
@@ -82,6 +82,8 @@ demand_response: 'moderate'
 # capex_mw_mile: Capital cost per MW-mile for all (numeric) or each model region (dict).
 # WACC and investment years from https://www.sciencedirect.com/science/article/abs/pii/S0301421519305816
 transmission_investment_cost:
+  # If true, uses interconnect_annuity when available
+  use_total: true
   spur:
     capex_mw_mile:
       CA_N: 8904

commit 5f03813b28eb3a2b64dfcefbadaabd1a8e5cf410

Add renewables clusters profiles to CLI output
- Store cluster profiles as resource `variability`
- Apply `setings.generators_columns` for writing to file only
- Remove `Commit = 0` if `Cap_size == 1` (overrides cluster capacity)
- Simplify `make_generator_variability()` logic using merge.
A limitation is that matching on `Resource` is now exact.
Only missing `variability` are filled from file.
- Remove renewables ('offshorewind', 'landbasedwind', and 'utilitypv') from
`test_variable_resource_profiles.csv`
- Fix syntax error in `test_settings.yml` introduced in b99263b

powergenome/external_data.py
@@ -1,6 +1,7 @@
 # Read in and add external inputs (user-supplied files) to PowerGenome outputs
 
 import logging
+import numpy as np
 import pandas as pd
 from pathlib import Path
 
@@ -149,23 +150,28 @@ def add_resource_max_cap_spur(new_resource_df, settings, capacity_col="Max_Cap_M
     return new_resource_df
 
 
-def make_generator_variability(resource_df, settings, resource_col="Resource"):
+def make_generator_variability(resource_df, settings):
     """Make a generator variability dataframe with normalized (0-1) hourly profiles
-    for each resource in resource_df. Any resources that are not supplied in the user
-    file are assumed to have constant hourly profiles with a value of 1.
+    for each resource in resource_df.
+    
+    Any resources that are not supplied in the file or have a profile in column
+    `variability` are assumed to have constant hourly profiles with a value of 1.
+    February 29 is removed from any profiles of length 8784 (leap year).
+
+    Matching between the file and resources in `resource_df` are done by:
+        - region (exact, case-sensitive)
+        - Resource (exact after conversion to snake case)
 
     Parameters
     ----------
     resource_df : DataFrame
-        All resources (new and existing), with a single row for each resource. Must have
-        columns of `region` and `cluster` in addition to a resource/technology column
-        that can be changed with the parameter `resource_col`.
+        All resources (new and existing), with a single row for each resource.
+        Must have columns `region`, `Resource`, `cluster`, and
+        (optional) `variability` which takes precedence over the file.
     settings : dict
         User-defined parameters from a settings file. Should have keys of `input_folder`
         (a Path object of where to find user-supplied data) and
         `resource_variability_fn` (the file to load).
-    resource_col : str, optional
-        Name of the column with resource/technology names, by default "Resource"
 
     Returns
     -------
@@ -175,82 +181,53 @@ def make_generator_variability(resource_df, settings, resource_col="Resource"):
 
     Raises
     ------
-    KeyError
+    MergeError
         More than one match from the user supplied data was found for a resource in
-        `resource_df`. These matches are done using str.contains() with names that have
-        been transformed to snake case. e.g. the user file might have `PV` as a resource
-        and `resource_df` might have both `UtilityPV` and `IndustrialPV`.
+        `resource_df`.
     """
 
     # Load resource variability (8760 values) and make it into a df with columns
     # 'region', 'Resource', 'cluster', and then the hourly values.
     path = Path(settings["input_folder"]) / settings["resource_variability_fn"]
     df = pd.read_csv(path, index_col=0)
-    df.index = [str(x) for x in df.index]
-    df = df.T
-
-    region_list = [x.split(".")[0] for x in df.index]
-    df = df.reset_index()
-    df.rename(columns={"index": "region"}, inplace=True)
-    df["region"] = region_list
-    df["Resource"] = snake_case_col(df["Resource"])
-    df["cluster"] = df["cluster"].fillna(1)
-
-    # Make a modified version of resource_df, where a new _resource column is populated
-    # with the names from generator variability. Resource has already been snake_case
-    # transformed.
-    df_list = []
-    for resource, _df in resource_df.groupby("Resource"):
-        _df["cluster"] = _df["cluster"].fillna(1)
-        count = 0
-        for r in df.Resource.unique():
-            if r.lower() in resource.lower():
-                _df["_resource"] = r
-                count += 1
-        if count > 1:
-            raise KeyError(
-                f"More than one name match was found for {resource} in the generator"
-                " variability file."
-            )
-        df_list.append(_df)
-    mod_resource_df = pd.concat(df_list)
-    mod_resource_df = mod_resource_df.sort_values("R_ID")
-
-    keep_cols = ["Resource_x"] + [str(x) for x in range(1, 8761)]
-
-    gen_variability = (
-        mod_resource_df.merge(
-            df.reset_index(drop=True),
-            left_on=["region", "_resource", "cluster"],
-            right_on=["region", "Resource", "cluster"],
-            how="left",
-        )
-        .loc[:, keep_cols]
-        .fillna(1)
-        .T
+    df = pd.DataFrame(
+        {
+            "region": df.columns.str.replace(r"\.[0-9]+$", ""),
+            "Resource": snake_case_col(df.iloc[0]).values,
+            "cluster": df.iloc[1].fillna(1).values,
+            "variability": list(df.iloc[2:].values.astype(float).T),
+        }
     )
 
-    gen_variability = gen_variability.rename(index={"Resource_x": "Resource"})
-    gen_variability.columns = [
-        f"{r}_{idx + 1}" for idx, r in enumerate(gen_variability.loc["Resource", :])
-    ]
-    gen_variability = gen_variability.drop(index="Resource")
-    gen_variability.index = [int(x) for x in gen_variability.index]
-
-    gen_variability = gen_variability.astype(float)
-    assert (
-        any(gen_variability.isna().any()) is False
-    ), "Something went wrong creating the gen variability file. The data has some NaN values"
-
-    # Check to make sure no variable resources have identical hourly profiles
-    for r in df.Resource.unique():
-        for col in [x for x in gen_variability.columns if r in x]:
-            if gen_variability[col].std == 0:
-                logger.warning(
-                    f"Column {col} in the gen varability df has a standard deviation of 0."
-                )
-
-    return gen_variability
+    merge = pd.merge(
+        resource_df.fillna({"cluster": 1}),
+        df,
+        how="left",
+        on=["region", "Resource", "cluster"],
+        suffixes=["__x", "__y"],
+        validate="many_to_one",
+    ).sort_values("R_ID")
+    if "variability__x" in merge:
+        # Fill missing with variability from file
+        # TODO: Ensure missing always NaN, not also 0, ...
+        missing = ~merge["variability__x"].apply(isinstance, args=[np.ndarray])
+        merge.loc[missing, "variability__x"] = merge.loc[missing, "variability__y"]
+        merge = merge.rename(columns={"variability__x": "variability"}).drop(
+            columns=["variability__y"]
+        )
+    # Fill missing with default (1)
+    merge["variability"] = merge["variability"].map(
+        lambda x: x if isinstance(x, np.ndarray) else np.ones(8760, dtype=float)
+    )
+    # Remove February 29 from leap year
+    merge["variability"] = merge["variability"].map(
+        lambda x: np.delete(x, slice(1416, 1440)) if len(x) == 8784 else x
+    )
+    return pd.DataFrame(
+        np.column_stack(merge["variability"]),
+        columns=merge[["R_ID", "Resource"]].astype(str).agg("_".join, axis=1),
+        index=range(1, 8761),
+    )
 
 
 def load_policy_scenarios(settings):

powergenome/generators.py
@@ -1190,9 +1190,6 @@ def add_genx_model_tags(df, settings):
         mask = technology.str.contains(fr"^{tech}", case=False)
         df.loc[(df["region"] == region) & mask, tag_col,] = tag_value
 
-    # Make unit size = 1 where Commit = 0 to avoid GenX bug
-    df.loc[df["Commit"] == 0, "Cap_size"] = 1
-
     return df
 
 
@@ -2343,10 +2340,4 @@ class GeneratorClusters:
                 f"Capacity of {self.all_resources['Existing_Cap_MW'].sum()} MW in final clusters"
             )
 
-        cols = [
-            col
-            for col in self.settings["generator_columns"]
-            if col in self.all_resources
-        ]
-
-        return self.all_resources.reindex(columns=cols)
+        return self.all_resources

powergenome/nrelatb.py
@@ -677,7 +677,6 @@ def atb_new_generators(atb_costs, atb_hr, settings):
         "Cap_size",
         "cap_recovery_years",
         "waccnomtech",
-        "regional_cost_multiplier",
     ]
     new_gen_df = new_gen_df[keep_cols]
 
@@ -795,10 +794,11 @@ def add_renewables_clusters(
         technology = technologies[0]
         builder = ClusterBuilder(SETTINGS["RENEWABLES_CLUSTERS"])
         builder.build_clusters(**scenario, ipm_regions=ipm_regions)
+        profiles = builder.get_cluster_profiles()
         clusters = (
             builder.get_cluster_metadata()
             .rename(columns={"mw": "Cap_size"})
-            .assign(technology=technology)
+            .assign(technology=technology, variability=list(profiles))
         )
         row = df[df["technology"] == technology].iloc[0]
         kwargs = {k: v for k, v in row.items() if k not in clusters}

powergenome/renewables_clusters.py
@@ -245,17 +245,14 @@ class ClusterBuilder:
             dfs.append(df)
         return pd.concat(dfs, axis=0, ignore_index=True, sort=False)
 
-    def get_cluster_profiles(self) -> Optional[pd.DataFrame]:
+    def get_cluster_profiles(self) -> np.ndarray:
         """
         Return computed cluster profiles.
 
-        Uses multi-index columns ('region', 'Resource', 'cluster'), where
-        - `region` (str): Region label passed to :meth:`build_clusters`.
-        - `Resource` (str): From resource metadata 'technology'.
-        - `cluster` (int): Unique identifier for each cluster to link to other outputs.
-
-        The first column is the hour number.
-        Subsequent columns are the cluster profiles.
+        Returns
+        -------
+        np.ndarray
+            Hourly normalized (0-1) generation profiles (n clusters, m hours).
 
         Raises
         ------
@@ -263,21 +260,7 @@ class ClusterBuilder:
             No clusters have yet been computed.
         """
         self._test_clusters_exist()
-        profiles = np.column_stack([c["profiles"] for c in self.clusters])
-        columns = []
-        start = 0
-        for c in self.clusters:
-            for _ in range(c["profiles"].shape[1]):
-                columns.append((c["region"], c["group"]["technology"], start))
-                start += 1
-        df = pd.DataFrame(profiles, columns=columns)
-        # Insert hour numbers into first column
-        df.insert(
-            loc=0,
-            column=("region", "Resource", "cluster"),
-            value=np.arange(HOURS_IN_YEAR),
-        )
-        return df
+        return np.row_stack([c["profiles"] for c in self.clusters])
 
 
 def load_groups(path: str = ".") -> List[dict]:
@@ -408,7 +391,7 @@ def build_cluster_profiles(
     path: str, clusters: pd.DataFrame, metadata: pd.DataFrame
 ) -> np.ndarray:
     """Build cluster profiles."""
-    results = np.zeros((HOURS_IN_YEAR, len(clusters)), dtype=float)
+    results = np.zeros((len(clusters), HOURS_IN_YEAR), dtype=float)
     for i, cids in enumerate(clusters["ids"]):
         weights = metadata.loc[cids, WEIGHT].values
         weights /= weights.sum()
@@ -421,7 +404,7 @@ def build_cluster_profiles(
             )
             # Assumes profile is already sorted by hour (ascending)
             df = read_parquet(path, filters=filters, columns=["capacity_factor"])
-            results[:, i] += df["capacity_factor"].values * weights[j]
+            results[i] += df["capacity_factor"].values * weights[j]
     return results
 
 

powergenome/run_powergenome_multiple_outputs_cli.py
@@ -359,8 +359,9 @@ def main():
                     )
                     # if settings.get("partial_ces"):
                     gens = calculate_partial_CES_values(gen_clusters, fuels, _settings)
+                    cols = [c for c in _settings["generator_columns"] if c in gens]
                     write_results_file(
-                        df=remove_fuel_scenario_name(gens.fillna(0), _settings),
+                        df=remove_fuel_scenario_name(gens[cols].fillna(0), _settings),
                         folder=case_folder,
                         file_name="Generators_data.csv",
                         include_index=False,
@@ -449,8 +450,9 @@ def main():
                         settings=_settings,
                     )
                     gens = calculate_partial_CES_values(gen_clusters, fuels, _settings)
+                    cols = [c for c in _settings["generator_columns"] if c in gens]
                     write_results_file(
-                        df=remove_fuel_scenario_name(gens.fillna(0), _settings),
+                        df=remove_fuel_scenario_name(gens[cols].fillna(0), _settings),
                         folder=case_folder,
                         file_name="Generators_data.csv",
                         include_index=False,

example_system/test_settings.yml
@@ -1156,7 +1156,7 @@ generator_columns: [
             "spur_miles",
             "spur_capex",
             "offshore_spur_miles",
-            "offshore_spur_capex"
+            "offshore_spur_capex",
             "tx_miles",
             "tx_capex"
         ]