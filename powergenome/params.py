6182892d1902545e34d991477e7336f842077de3
gschivley <gschivley@users.noreply.github.com>
2020-08-29 17:44:16 -0400

    Remove RESOURCE_GROUPS key if None

--- powergenome/params.py
+++ powergenome/params.py
@@ -36,6 +36,8 @@ SETTINGS = {}
 SETTINGS["PUDL_DB"] = os.environ.get("PUDL_DB")
 SETTINGS["EIA_API_KEY"] = os.environ.get("EIA_API_KEY")
 SETTINGS["RESOURCE_GROUPS"] = os.environ.get("RESOURCE_GROUPS")
+if not SETTINGS["RESOURCE_GROUPS"]:
+    SETTINGS.pop("RESOURCE_GROUPS", None)
 
 CLUSTER_BUILDER = ClusterBuilder.from_json(
     Path(SETTINGS.get("RESOURCE_GROUPS"), ".").glob("**/*.json")
