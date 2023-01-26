b33db3bd1982f14db2eed7a57d01784230da6fa4
Greg Schivley <greg@carbonimpact.co>
2023-01-26 13:17:04 -0500

    Bug fix: Use empty dict if Min/MaxCapReq is None

--- powergenome/GenX.py
+++ powergenome/GenX.py
@@ -923,7 +923,7 @@ def min_cap_req(settings: dict) -> pd.DataFrame:
     min_mw = []
 
     # if settings.get("MinCapReq"):
-    for cap_tag, values in settings.get("MinCapReq", {}).items():
+    for cap_tag, values in (settings.get("MinCapReq", {}) or {}).items():
         if cap_tag not in settings.get("model_tag_names", []):
             raise KeyError(
                 f"The minimum capacity tag {cap_tag} is listed in the settings "
@@ -983,7 +983,7 @@ def max_cap_req(settings: dict) -> pd.DataFrame:
     max_mw = []
 
     # if settings.get("MaxCapReq"):
-    for cap_tag, values in settings.get("MaxCapReq", {}).items():
+    for cap_tag, values in (settings.get("MaxCapReq", {}) or {}).items():
         if cap_tag not in settings.get("model_tag_names", []):
             raise KeyError(
                 f"The maximum capacity tag {cap_tag} is listed in the settings "
