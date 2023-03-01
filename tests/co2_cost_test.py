c890914f0ab608156d752a1b4339aae37b3b2dbe
Greg Schivley <greg@carbonimpact.co>
2023-03-01 09:45:37 -0500

    Simplify co2 cost test
    Don't run merge_co2_pipeline_costs in test for a different function.
    Build the required dataframe and run it through the add_co2_costs func.

--- tests/co2_cost_test.py
+++ tests/co2_cost_test.py
@@ -76,26 +76,14 @@ def test_add_co2_costs_genx():
         "Heat_Rate_MMBTU_per_MWh": [5000, 8000, 8000],
         "Fuel": ["None", "coal", "coal"],
     }
+    co2_cost_data = {
+        "co2_o_m_mw": [0, 2814, 471],
+        "co2_pipeline_annuity_mw": [0, 8670, 1457],
+        "co2_cost_mwh": [0, 19614, 17813],
+    }
     df = pd.DataFrame(data)
-    region_aggregations = {"ERCOT": ["ERC_WEST", "ERC_REST"]}
-    co2_pipeline_filters = [
-        {
-            "technology": "Coal",
-            "tech_detail": "CCS90AvgCF",
-            "with_backbone": True,
-            "percentile": 25,
-        }
-    ]
-    fuel_emission_factors = {"coal": 0.09552}
-
-    merge_df = merge_co2_pipeline_costs(
-        df,
-        DATA_FOLDER / "co2_pipeline_cost_percentiles.csv",
-        co2_pipeline_filters,
-        region_aggregations,
-        fuel_emission_factors,
-        target_usd_year=2020,
-    ).pipe(add_co2_costs_to_o_m)
+    merge_df = pd.concat([df, pd.DataFrame(co2_cost_data)], axis=1)
+    merge_df = add_co2_costs_to_o_m(merge_df)
     cost_cols = ["Var_OM_Cost_per_MWh", "Inv_Cost_per_MWyr", "Fixed_OM_Cost_per_MWyr"]
     assert np.allclose(df.loc[0, cost_cols].sum(), merge_df.loc[0, cost_cols].sum())
     assert (df.loc[1:, cost_cols].sum() < merge_df.loc[1:, cost_cols].sum()).all()
