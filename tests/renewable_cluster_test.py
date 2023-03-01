b9c2c872c0ff7972899c19d31acb05f69b742663
Greg Schivley <greg@carbonimpact.co>
2023-03-01 14:49:54 -0500

    Add test for assign_site_cluster

--- tests/renewable_cluster_test.py
+++ tests/renewable_cluster_test.py
@@ -1,8 +1,11 @@
 "Test functions for clustering renewable sites"
 
+from pathlib import Path
+
 import pandas as pd
 
 from powergenome.cluster.renewables import (
+    assign_site_cluster,
     value_bin,
     agg_cluster_other,
     agg_cluster_profile,
@@ -15,6 +18,8 @@ from hypothesis import given, strategies as st
 from hypothesis.extra.pandas import data_frames, column, range_indexes, series
 from hypothesis.extra.numpy import arrays
 
+CWD = Path.cwd()
+DATA_FOLDER = CWD / "tests" / "data" / "cpa_cluster_data"
 
 cluster_data = data_frames(
     columns=[
@@ -103,3 +108,70 @@ def test_fuzz_agglomerative_cluster_binned(data, feature, by, n_clusters):
     agglomerative_cluster_binned(
         data=data, by=by, feature=feature, n_clusters=n_clusters
     )
+
+
+def test_assign_site_cluster():
+    renew_data = pd.read_csv(DATA_FOLDER / "cpa_data.csv")
+    profile_path = DATA_FOLDER / "cpa_profiles.csv"
+    regions = ["A", "B"]
+    cluster = {
+        "min_capacity": 2000,
+        "filter": [
+            {
+                "feature": "lcoe",
+                "max": 49,
+            }
+        ],
+        "bin": [{"feature": "interconnect_annuity", "bins": 2}],
+        "group": ["county"],
+        "cluster": [
+            {"feature": "lcoe", "method": "agg", "n_clusters": 2},
+        ],
+    }
+
+    data = assign_site_cluster(
+        renew_data=renew_data, profile_path=profile_path, regions=regions, **cluster
+    )
+    assert data.notna().all().all()
+    assert "cluster" in data.columns
+
+    cluster = {
+        "cluster": [
+            {"feature": "profile", "method": "hierarchical", "n_clusters": 3},
+        ],
+    }
+    data = assign_site_cluster(
+        renew_data=renew_data, profile_path=profile_path, regions=regions, **cluster
+    )
+    assert data.notna().all().all()
+    assert "cluster" in data.columns
+
+    cluster = {
+        "bin": [{"feature": "interconnect_annuity", "mw_per_bin": 200}],
+    }
+    data = assign_site_cluster(
+        renew_data=renew_data, profile_path=profile_path, regions=regions, **cluster
+    )
+    assert data.notna().all().all()
+    assert "cluster" in data.columns
+
+    cluster = {
+        "bin": [{"feature": "interconnect_annuity", "mw_per_q": 200}],
+    }
+    data = assign_site_cluster(
+        renew_data=renew_data, profile_path=profile_path, regions=regions, **cluster
+    )
+    assert data.notna().all().all()
+    assert "cluster" in data.columns
+
+    cluster = {
+        "cluster": [
+            {"feature": "profile", "method": "agglomerative", "mw_per_cluster": 200},
+        ],
+    }
+    data = assign_site_cluster(
+        renew_data=renew_data, profile_path=profile_path, regions=regions, **cluster
+    )
+    assert data.notna().all().all()
+    assert "cluster" in data.columns
+    assert len(data) == len(renew_data)
