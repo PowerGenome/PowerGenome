860f55610800033210fe69613ae7ac456c88dd3a
Greg Schivley <greg@carbonimpact.co>
2023-03-03 09:47:40 -0500

    Bug fix: simplify finding variable gen types
    The old method failed for an unknown reason. Using a more pandas-esq
    approach.

--- powergenome/time_reduction.py
+++ powergenome/time_reduction.py
@@ -67,8 +67,9 @@ def kmeans_time_clustering(
     resource_col_names = resource_profiles.columns
     if variable_resources_only:
         input_std = resource_profiles.describe().loc["std", :]
-        var_col_names = [col for col in input_std.index if input_std[col] > 0]
+        var_col_names = input_std[input_std > 0].index.to_list()
         resource_profiles = resource_profiles.loc[:, var_col_names]
+
     # Initialize dataframes to store final and intermediate data in
 
     input_data = pd.concat(
