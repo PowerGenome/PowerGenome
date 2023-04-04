c6163633aad90649d62ab01308bf66fcecca639b
Greg Schivley <greg@carbonimpact.co>
2023-04-04 14:32:38 -0400

    Add tests for num bins from capacity

--- tests/renewable_cluster_test.py
+++ tests/renewable_cluster_test.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from powergenome.cluster.renewables import (
     assign_site_cluster,
+    num_bins_from_capacity,
     value_bin,
     agg_cluster_other,
     agg_cluster_profile,
@@ -175,3 +176,55 @@ def test_assign_site_cluster():
     assert data.notna().all().all()
     assert "cluster" in data.columns
     assert len(data) == len(renew_data)
+
+
+# Tests that the function correctly calculates the number of bins based on the "mw_per_bin" key in the input dictionary. tags: [happy path]
+def test_num_bins_from_capacity_with_mw_per_bin():
+    # Happy path test for calculating number of bins based on "mw_per_bin"
+    data = pd.DataFrame({"mw": [10, 20, 30]})
+    b = {"mw_per_bin": 14}
+    expected_output = {"bins": 4}
+    assert num_bins_from_capacity(data, b) == expected_output
+
+
+def test_num_bins_from_capacity_with_mw_per_bin_one_bin():
+    # Happy path test for calculating number of bins based on "mw_per_bin"
+    data = pd.DataFrame({"mw": [1, 2, 3]})
+    b = {"mw_per_bin": 14}
+    expected_output = {"bins": 1}
+    assert num_bins_from_capacity(data, b) == expected_output
+
+
+# Tests that the function correctly calculates the number of quantiles based on the "mw_per_q" key in the input dictionary. tags: [happy path]
+def test_num_bins_from_capacity_with_mw_per_q():
+    # Happy path test for calculating number of quantiles based on "mw_per_q" key
+    data = pd.DataFrame({"mw": [10, 20, 30]})
+    b = {"mw_per_q": 15}
+    expected_output = {"q": 4}
+    assert num_bins_from_capacity(data, b) == expected_output
+
+
+# Tests that the function returns the input dictionary unaltered if neither "mw_per_bin" nor "mw_per_q" key is present. tags: [happy path]
+def test_num_bins_from_capacity_with_no_mw_key():
+    # Happy path test for returning input dictionary unaltered if no "mw_per_bin" or "mw_per_q" key is present
+    data = pd.DataFrame({"mw": [10, 20, 30]})
+    b = {"other_key": "value"}
+    expected_output = {"other_key": "value"}
+    assert num_bins_from_capacity(data, b) == expected_output
+
+
+# Tests that the function handles input dictionary containing non-integer values for "mw_per_bin" or "mw_per_q". tags: [edge case]
+def test_num_bins_from_capacity_with_non_integer_values():
+    data = pd.DataFrame({"mw": [100, 200, 300]})
+    b = {"mw_per_bin": 0.5}
+    result = num_bins_from_capacity(data, b)
+    assert result == {"bins": 1200}
+
+
+# Tests that the function logs a warning message if the "bins" key is already present in the input dictionary and is being overwritten. tags: [behavior]
+def test_num_bins_from_capacity_with_overwriting_bins(caplog):
+    data = pd.DataFrame({"mw": [100, 200, 300]})
+    b = {"mw_per_bin": 100, "bins": 5}
+    result = num_bins_from_capacity(data, b)
+    assert result == {"bins": 6}
+    assert "Overwriting 'bins' based on mw_per_bin" in caplog.text
