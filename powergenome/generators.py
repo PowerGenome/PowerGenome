574b5bc24db649e1d34976d05a1ce05ba6d38ea4
Greg Schivley <greg.schivley@gmail.com>
2024-05-03 10:26:07 -0400

    Improve multi-period file setup
    Default to consistent clustering across periods without needing an extra settings parameter

--- powergenome/generators.py
+++ powergenome/generators.py
@@ -3406,19 +3406,30 @@ class GeneratorClusters:
         self.retired_index = self.retired.set_index(
             ["plant_id_eia", "unit_id_pg"]
         ).index
-        if self.settings.get("cluster_with_retired_gens", False) is True:
+        if (
+            self.multi_period
+            and self.settings.get("cluster_with_retired_gens", True) is True
+        ):
             logger.info(
-                "\n\nAge-retired gens are included for clustering to keep consistent "
-                "cluster assignments across periods. \n\n"
+                "Age-retired gens are included for clustering to keep consistent "
+                "cluster assignments across periods. "
+            )
+            region_tech_grouped = self.units_model.loc[
+                self.units_model.technology.isin(techs), :
+            ].groupby(["model_region", "technology"])
+        elif self.settings.get("cluster_with_retired_gens", False) is True:
+            logger.info(
+                "Age-retired gens are included for clustering to keep consistent "
+                "cluster assignments across periods. "
             )
             region_tech_grouped = self.units_model.loc[
                 self.units_model.technology.isin(techs), :
             ].groupby(["model_region", "technology"])
         else:
             logger.info(
-                "\n\nAge-retired gens are NOT included for clustering. This may lead to "
+                "Age-retired gens are NOT included for clustering. This may lead to "
                 "inconsistent cluster assignments across periods. If you want to change "
-                "this behavior, add 'cluster_with_retired_gens: true' to your settings.\n\n"
+                "this behavior, add 'cluster_with_retired_gens: true' to your settings."
             )
             region_tech_grouped = self.units_model.loc[
                 (self.units_model.technology.isin(techs))
